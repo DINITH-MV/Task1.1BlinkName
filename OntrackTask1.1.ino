91bd4dffa7e0a601dc8bb0f448c9e6b86cc51f1b
dinith vanderlan <dinithvanderlan@gmail.com>
2025-03-07 22:03:34 +1100

    Update OntrackTask1.1.ino

--- OntrackTask1.1.ino
+++ OntrackTask1.1.ino
@@ -1,36 +1,29 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // use the Built-In LED
 }
 
-// D  -..
-// I  ..
-// N  -.
-// I  ..
-// T  -
-// H  ....
-
-String name[6] = {"-..", "..", "-.", "..", "-", "...."};
+String name[6][2] = {{"D","-.."}, {"I",".."}, {"N","-."}, {"I",".."}, {"T","-"}, {"H","...."}}; // Morse codes according to the name
 
 void loop() {
 for (int a = 0; a < 6; a++) {
-  String letter = name[a];
+  String letter = name[a][2]; // assign the element to the "letter" variable
   for (int b = 0; b < letter.length(); b++) {
-    if(letter[b] == '.') {
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(300);  
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(400);
+    if(letter[b] == '.') { // check whether the characters of the element same as "."
+      digitalWrite(LED_BUILTIN, HIGH); // make LED turn on
+      delay(300); // keeps LED on for 300ms
+      digitalWrite(LED_BUILTIN, LOW); // make LED turn on
+      delay(400); // holds LED off for 400ms 
     }
 
-    else if(letter[b] == '-') {
+    else if(letter[b] == '-') { // check whether the characters of the elements same as ","
       digitalWrite(LED_BUILTIN, HIGH);
       delay(900);  
       digitalWrite(LED_BUILTIN, LOW);
       delay(400);
     }
   }
-  delay(600);
+  delay(600); // Holds LED off for 600ms for every element
   }
-  delay(1800);
+  delay(1800); // Holds LED off for 1800ms for every word
 }
 
